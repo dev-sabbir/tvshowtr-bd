222a440dbe8a4ba1f6b6d756c57b852d87a3f7e0
InsomniacSabbir <sabbiralam.cse@gmail.com>
2016-10-10 00:12:50 +0600

    Form validation

--- hello.py
+++ hello.py
@@ -4,19 +4,25 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
-
+msg =""
 @app.route("/")
 def hello():
     l = get_all()
-    return render_template("index.html",l=l)
+    global msg
+    return render_template("index.html",l=l,msg=msg)
 
 
 @app.route("/add",methods = ["POST","GET"])
 def add():
-    if request.method == 'POST':
+    global msg
+    msg=""
+    if request.method == 'POST' and request.form['title']!="":
         title = request.form['title']
         print title
         rows = add_tv(title)
+        return redirect(url_for('hello'))
+    else:
+        msg = "Don't insert wrong input. Try something like this: tt3107288"
         return redirect(url_for('hello'))
 if __name__ == "__main__":
             app.run(debug=True)
